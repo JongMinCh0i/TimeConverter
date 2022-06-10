7ccce7c5a11ce16199541262991d699c83859c92
hyunho12 <hyundho12@gmail.com>
2022-06-10 18:10:27 +0900

    FEAT : Turkey 시차계산 기능추가

--- src/main/java/TimeZoneConverter.java
+++ src/main/java/TimeZoneConverter.java
@@ -37,6 +37,46 @@ public class TimeZoneConverter {
         else if ("터키".equals(str)) {
             // 개인처리 내부 메서드 구현
             // 브런치 이름은 개인별 이니셜로 구현
+            int t_year = getYear();
+            int t_month = getMonth();
+            int t_day = getDay();
+            int t_hour = getHour() - 6;
+
+            if(getHour() < 6){
+                t_hour = 24 + t_hour;
+                if(getDay() == 1){
+                    if (t_month == 2 || t_month == 4 || t_month == 6 || t_month == 9 || t_month == 11) {
+                        t_day = 31;
+                        t_month = t_month - 1;
+                    } else if (t_month == 5 || t_month == 7 || t_month == 8 || t_month == 10 || t_month == 12) {
+                        t_day = 30;
+                        t_month = t_month - 1;
+                    } else if (t_month == 1) {
+                        t_day = 31;
+                        t_month = 12;
+                        t_year = t_year - 1;
+                    } else if(t_month == 3) {
+                        t_day = 28;
+                        t_month = t_month - 1;
+                    }
+                }
+                else if((t_month == 1 || t_month == 3 || t_month == 5 || t_month == 7 || t_month == 8 || t_month == 10 || t_month == 12) && (t_day > 31 || t_day < 1)){
+                    System.out.println("날짜 입력 오류 !! \n입력 월의 날짜는 1~31까지있습니다.");
+                }
+                else if((t_month == 4 || t_month == 6 || t_month == 9 || t_month == 11) && (t_day > 30 || t_day < 1)){
+                    System.out.println("날짜 입력 오류 !! \n입력 월의 날짜는 1~30까지있습니다.");
+
+                }
+                else if(t_month == 2 && (t_day > 28 || t_day < 1)){
+                    System.out.println("날짜 입력 오류 !! \n입력 월의 날짜는 1~28까지있습니다.");
+                }
+                else{
+                    t_day = t_day - 1;
+                }
+            }
+
+            System.out.println("Input :"+ getYear() + "-" + getMonth() + "-" + getDay() + "," + getHour()  + ":" + getMin());
+            System.out.println("Turkey :" + t_year + "-" + t_month + "-" + t_day + "," + t_hour + ":" + getMin());
         }
         else if ("프랑스".equals(str)) {
             // 개인처리 내부 메서드 구현
